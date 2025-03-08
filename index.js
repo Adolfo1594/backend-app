03452c4632d318d31b39c438ddffb5d36ff055ba
Adolfo1594 <utp0159427@alumno.utpuebla.edu.mx>
2025-03-08 11:34:53 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+//Incocación de la libreria express
+const  express = require('express');
+//Invoncación de la librería moongose para conexión a base de datos de mongodb
+const mongoose = require('mongoose');
+
+
+// Importar las rutas de la colección review
+const reviewWine = require('./routes/wine.route');
+
+
+//inicialización de la aplicación basada en express
+const app = express();
+app.use(express.json());
+
+// Determinación del puerto
+//Tareas CRUD
+// C - Create (Crear, agregar) ---> post
+// R - Read (Leer o recuperar) ---> get
+// U - Update (Actualizar) -------> put
+// D - Delete (borrar o eliminar)-> delete
+
+// Endpoint por defecto
+app.get('/',(req, res) => {
+    res.send('Bienvenido a mi servidor de APIs');
+});
+
+
+//Endpoints de la colección reviews
+app.use('/api/wine',reviewWine);
+
+
+// Definiendo la conexión a la base de datos a trtavés de mongoose
+// La conexión de tipo de datos es de tipo promesa
+
+mongoose.connect('mongodb://localhost:27017/wine_db')
+.then( () => {
+    //realiza tareas una vez la promesa sea exitosa
+    console.log('Conectado a la base de datos de manera exitosa');
+    app.listen(3000, () => {
+    console.log('Servidor respondiendo en el puerto 3000');
+    });
+})
+
+.catch(() => console.log('Ocurrio un problema al conectarse a la base de datos'))
+
